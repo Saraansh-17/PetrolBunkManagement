da02416e4b95bcfa807f83506fcd8c550db1a71e
pallavi9931 <pallavi.kumari.cs.2022@mitmeerut.ac.in>
2026-05-12 11:12:20 +0530

    updated hr module

---
+++ frontend/src/modules/hr/index.js
@@ -0,0 +1,10 @@
+import Dashboard from './pages/Dashboard';
+import Employees from './pages/Employees';
+import Attendance from './pages/Attendance';
+import './styles/hr-styles.css';
+
+export {
+  Dashboard as HRDashboard,
+  Employees as HREmployees,
+  Attendance as HRAttendance
+};
